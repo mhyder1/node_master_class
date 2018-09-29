a8747dc83e79c8c88e3dbd25230be3d700dfa09b
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2018-09-29 08:35:57 -0500

    fixing broken branches

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -4,12 +4,29 @@ Primary file for API
 
 //Dependencies
 const http = require('http');
+const url = require('url');
 
 //The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
 
+    // Get the URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Get the query string as an object i.e. ?fizz=buzz&foo=bar
+    const queryStringObject = parsedUrl.query;
+
+    // Get http method i.e. GET, POST, PUT, DELETE
+    const method = req.method.toLowerCase();
+
     // Send the response
     res.end('Hello World\n');
+
+    // Lod the request path
+    console.log('Request received on path: '+trimmedPath+' with method: '+ method+' and with these query string parameters:', queryStringObject);
 })
 
 
