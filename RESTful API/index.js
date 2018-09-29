62e11f285893c2a3b21f98845f82a91867c54d3b
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2018-09-29 09:03:35 -0500

    finished starting server

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -4,31 +4,13 @@ Primary file for API
 
 //Dependencies
 const http = require('http');
-const url = require('url');
 
 //The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
 
-    // Get the URL and parse it
-    const parsedUrl = url.parse(req.url, true);
-
-    // Get the path
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-    // Get the query string as an object
-    const queryStringObject = parsedUrl.query;
-
-    // Get http method
-    const method = req.method.toLowerCase();
-
     // Send the response
     res.end('Hello World\n');
-
-    // Lod the request path
-    console.log('Request received on path: '+trimmedPath+' with method: '+ method+' and with these query string parameters:', queryStringObject);
 })
-
 
 //Start the server, and hav it listen on prot 3000
 server.listen(3000, () => {
